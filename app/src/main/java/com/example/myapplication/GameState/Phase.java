3ea8870ef8779e65254a9081334e15b80a66072b
johnhaas <haasj22@up.edu>
2019-10-10 23:33:01 -0700

    added Citation

--- app/src/main/java/com/example/myapplication/GameState/Phase.java
+++ app/src/main/java/com/example/myapplication/GameState/Phase.java
@@ -6,6 +6,14 @@
 
 package com.example.myapplication.GameState;
 
+/**
+ * External Citation
+ * Person Writing: John Haas
+ * Problem: I did not know how to add an enum to a toString method
+ *
+ * Resource: https://www.java67.com/2014/12/2-ways-to-print-custom-string-value-of.html
+ * Solution: I used the sample code found on this site
+ */
 enum Phase {
     SETUP_PHASE {
         @Override
